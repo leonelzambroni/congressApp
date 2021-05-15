1f05f3ea1c78ef4240e7155ff49c894eb9c89121
Leonel Zambroni <leozambroni@gmail.com>
2021-05-15 15:04:30 -0300

    base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MemberDetailComponent } from './components/member-detail/member-detail.component';
+import { CongressMembersComponent } from './components/congress-members/congress-members.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MemberDetailComponent,
+    CongressMembersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/congress-members/congress-members.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-congress-members',
+  templateUrl: './congress-members.component.html',
+  styleUrls: ['./congress-members.component.scss']
+})
+export class CongressMembersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
