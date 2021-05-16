fbb414ff73513b1cbe2fe11547d4d9c135642cd2
Leonel Zambroni <leozambroni@gmail.com>
2021-05-15 22:45:57 -0300

    congressmen list screen wip

--- src/app/components/congress-members/congress-members.component.ts
+++ src/app/components/congress-members/congress-members.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { CongressService } from 'src/app/services/congress/congress.service';
 
 @Component({
   selector: 'app-congress-members',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CongressMembersComponent implements OnInit {
 
-  constructor() { }
+  congressInfo = [];
+  displayedColumns: string[] = ['title', 'name', 'party', 'state'];
+  data: any[] = [];
+  dataSource = new MatTableDataSource<any>(this.data);
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator | undefined;
+
+  constructor(private congressService: CongressService,) { }
 
   ngOnInit(): void {
+    this.getAllCongressmen();
+  }
+
+  getAllCongressmen() {
+    let congressmenData;
+    this.congressService.getAllCongressmen().subscribe(
+      res => {
+        console.log(res);
+        this.dataSource = new MatTableDataSource<any>(res.results[0].members);
+        if (this.paginator) {
+          this.dataSource.paginator = this.paginator;
+        }
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  getCongressMen() {
+    this.congressInfo
   }
 
 }
--- src/app/services/congress/congress.service.ts
+++ src/app/services/congress/congress.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CongressService {
 
-  constructor() { }
+
+  headers = new HttpHeaders({
+    'X-API-Key': '3AaiZHfaoIZXiwZnZrliAcJpKPwLPLMrpEcCIgBM',
+    'Content-Type': 'application/json; charset=utf-8',
+  });
+
+  stdJsonOptions = {
+    headers: this.headers
+  };
+
+  baseUrl = environment.baseUrl;
+
+  constructor(private _http: HttpClient) { }
+
+  getAllCongressmen() {
+    return this._http.get<any>(this.baseUrl + '/116/senate/members.json/', this.stdJsonOptions);
+  }
+
+  getCongressmen(){
+    
+  }
 }
