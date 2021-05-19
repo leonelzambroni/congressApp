f8f5d3ea549c6d7e88e28d7696ace5bbfae58a40
Leonel Zambroni <leozambroni@gmail.com>
2021-05-18 22:39:05 -0300

    server.js fix

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/congress-app'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/congress-app/index.html'));});
+'/index.html'));});
 app.listen(process.env.PORT || 8080);
